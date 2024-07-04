d48b62fac85c79d24a78b93bf0dd53f80e5973ea
Henning Bredel <h.bredel@52north.org>
2024-07-04 18:02:22 +0200

    Skips thumbnail generation on tabular data
    Closes: https://github.com/GeoNodeUserGroup-DE/geonode-dev-datapackage/issues/13

--- importer_datapackage/apps.py
+++ importer_datapackage/apps.py
@@ -1,10 +1,27 @@
 
 import logging
 
-from django.apps import AppConfig
+from django.apps import apps, AppConfig
 
 
 logger = logging.getLogger(__name__)
+
+
+_original_generator = None
+
+
+def run_setup_hooks(*args, **kwargs):
+    from django.conf import settings
+    _original_generator = settings.THUMBNAIL_GENERATOR
+    settings.THUMBNAIL_GENERATOR = "importer_datapackage.apps.create_thumbnail"
+
+
+def create_thumbnail(instance, overwrite=False, check_bbox=False):
+    if instance.subtype != "tabular":
+        _original_generator(instance, overwrite, check_bbox)
+    else:
+        # do nothing
+        pass
 
 
 class DataPackageConfig(AppConfig):
@@ -12,4 +29,6 @@ class DataPackageConfig(AppConfig):
     verbose_name = "Datapackage"
 
     def ready(self):
+        if not apps.ready:
+            run_setup_hooks()
         super(DataPackageConfig, self).ready()
